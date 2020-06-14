5513274bbedb0ff42b1a054cc4b1a1fc34646858
gminteer <gminteer@gmail.com>
2020-06-14 03:21:17 -0400

    pull quote in hero now a random unix fortune

--- assets/scripts/angularglue.js
+++ assets/scripts/angularglue.js
@@ -1,5 +1,5 @@
 var myApp = angular.module('myApp', []);
-
+/* can't directly attach angular.js to a random data source or it gets cranky */
 myApp.controller('about', ['$scope', function ($scope) {
     if(typeof d == 'undefined') {
         var d = {
@@ -14,6 +14,7 @@ myApp.controller('about', ['$scope', function ($scope) {
     $scope.longParagraph = d.longParagraph;
     $scope.paragraph = d.paragraph;
 }]);
+/* one double tile for the centrepiece */
 myApp.controller('doubleTile', ['$scope', function ($scope) {
     if(typeof d == 'undefined') {
         var d = {
@@ -24,65 +25,60 @@ myApp.controller('doubleTile', ['$scope', function ($scope) {
     $scope.title = d.title;
     $scope.text = d.text;
 }]);
-
+/* two wide tiles for secondary emphasis */
 for(i = 0; i < 2; i++ ) {
     myApp.controller('wideTile' + i, ['$scope', function ($scope) {
         if(typeof d == 'undefined') {
             var d = {
                 title: faker.commerce.productName(),
                 text: faker.lorem.sentence()
-            }
-        }
+            };
+        };
         $scope.title = d.title;
         $scope.text = d.text;
-    }])
-}
-
+    }]);
+};
+/* two tall tiles for things that require more wordy commentary and/or tertiary emphasis */
 for(i = 0; i < 2; i++ ) {
     myApp.controller('tallTile' + i, ['$scope', function ($scope) {
         if(typeof d == 'undefined') {
             var d = {
-                title: faker.hacker.adjective() + faker.hacker.noun(),
+                title: faker.hacker.adjective() + ' ' + faker.hacker.noun(),
                 text: faker.lorem.paragraph()
-            }
-        }
+            };
+        };
         $scope.title = d.title;
         $scope.text = d.text;
-    }])
-}
+    }]);
+};
+/* four "normal" tiles */
 for(i = 0; i < 4; i++ ) {
     myApp.controller('tile' + i, ['$scope', function ($scope) {
         if(typeof d == 'undefined') {
             var d = {
-                title: faker.hacker.adjective() + faker.hacker.noun(),
+                title: faker.hacker.adjective() + ' ' + faker.hacker.noun(),
                 text: faker.lorem.sentence()
-            }
-        }
+            };
+        };
         $scope.title = d.title;
         $scope.text = d.text;
-    }])
-}
-myApp.controller('fortuneData', ['$scope', function ($scope) {
-    var fortuneService = 'https://api.ef.gy/fortune';
-    var fortune = {
-        text: 'something went wrong :(',
-        source: 'This Website'
-    };
-    $.ajax({
-        headers: {
-            Accept: 'text/json',
-        },
-        url: fortuneService,
-        dataType: 'json',
-        jsonp: false,
-        success: function(data) {
-            $scope.fortune = data.cookie;
-            $scope.source = data.file + "#" + data['file-id'];
-            console.info($scope);
-        },
-        error: function(fortune, error, status) {
-            console.error(fortune, error, status);
-        }
+    }]);
+};
+/* grab a random fortune from a REST server some dude that works for Google set up as a personal joke */
+myApp.controller('fortuneData', function($scope, $http) {
+    $http.defaults.headers.common.Accept = 'text/json';
+    $http.get('https://api.ef.gy/fortune')
+    .then(function(response) { /* success */
+        /* censor things in /off/ because someone might get upset and the API doesn't let be exclude categories */
+        if(response.data.file.includes('/off/')) {
+            $scope.text = 'You miss 100% of the shots you don\'t take';
+            $scope.source = '- Albert Einstein';
+        } else {
+            $scope.text = response.data.cookie;
+            $scope.source = response.data.file + "#" + response.data['file-id']; /* it can't be "response.data.file-id" because of the "-" character */
+        };
+    }, function(response) { /* failure */
+        $scope.text = "something wen't wrong :("
+        $scope.source = "This Website"
     });
-    console.info("hi there");
-}]);
+});
