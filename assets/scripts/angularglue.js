268c38a98c0d22ee5969c23717b3de3400ed0e46
gminteer <gminteer@gmail.com>
2020-06-18 02:30:24 -0400

    fix for the last fix, some slight tweaks

--- assets/scripts/angularglue.js
+++ assets/scripts/angularglue.js
@@ -59,7 +59,7 @@ myApp.controller('fortuneData', function($scope, $http) {
         /* censor things in /off/ because someone might get upset and the API doesn't let me filter */
         if(response.data.file.includes('/off/')) {
             $scope.text = 'You miss 100% of the shots you don\'t take.';
-            $scope.source = 'Albert Einstein';
+            $scope.source = '- Albert Einstein';
         } else {
             $scope.text = response.data.cookie;
             $scope.source = response.data.file + "#" + response.data['file-id']; /* it can't be "response.data.file-id" because of the "-" character */
